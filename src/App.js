20c52faad18fe1a8929f8169885ad4de80ab6824
Igor Zevaka <igor.zevaka@nearmap.com>
2022-06-25 21:36:27 +1000

    Added API load with default city

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity="New York" />
         <footer>
           This project is created by Igor Zevaka and is{" "}
           <a
--- src/Weather.js
+++ src/Weather.js
@@ -1,49 +1,78 @@
-import React from "react";
+import axios from "axios";
+import React, { useState, useRef, useEffect } from "react";
 import "./Weather.css";
 
-export default function Weather() {
-  return (
-    <div className="Weather">
-      <form>
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Enter a city"
-              className="form-control"
-              autoFocus="on"
-            />
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  const loading = useRef(false);
+
+  function handleResponse(resp) {
+    console.log(resp);
+    loading.current = false;
+    const w = {
+      ready: true,
+      temperature: Math.round(resp.data.main.temp),
+      city: resp.data.name,
+      wind: Math.round(resp.data.wind.speed * 3.6),
+      humidity: resp.data.main.humidity,
+      description: resp.data.weather[0].description,
+      date: "Saturday 07:00",
+      iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+    };
+    setWeatherData(w);
+  }
+
+  useEffect(() => {
+    if (!loading.current) {
+      loading.current = true;
+      const apiKey = "f8c936db9cec49b0a939c31814fb3e34";
+      let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+      axios.get(apiUrl).then(handleResponse);
+    }
+  }, []);
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city"
+                className="form-control"
+                autoFocus="on"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
+            </div>
           </div>
-          <div className="col-3">
-            <input
-              type="submit"
-              value="Search"
-              className="btn btn-primary w-100"
-            />
+        </form>
+        <h1>{weatherData.city}</h1>
+        <ul>
+          <li>{weatherData.date}</li>
+          <li className="text-capitalize">{weatherData.description}</li>
+        </ul>
+        <div className="row mt-3">
+          <div className="col-6 temp-unit">
+            <img alt="cloudy" src={weatherData.iconUrl}></img>{" "}
+            <span className="temperature">{weatherData.temperature}</span>
+            <span className="unit">°C</span>
           </div>
-        </div>
-      </form>
-      <h1>New York</h1>
-      <ul>
-        <li>Saturday 15:00</li>
-        <li>Mostly Cloudy</li>
-      </ul>
-      <div className="row mt-3">
-        <div className="col-6 temp-unit">
-          <img
-            alt="cloudy"
-            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-          ></img>{" "}
-          <span className="temperature">16</span>
-          <span className="unit">°C</span>
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Humidiy: 53%</li>
-            <li>Wind: 12 km/h</li>
-          </ul>
+          <div className="col-6">
+            <ul>
+              <li>Humidiy: {weatherData.humidity}%</li>
+              <li>Wind: {weatherData.wind} km/h</li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
+  return "Loading";
 }
