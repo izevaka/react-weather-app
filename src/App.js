60963ccfb0298d01da92df699ae39717feeecb6f
Igor Zevaka <igor.zevaka@nearmap.com>
2022-06-25 15:16:54 +1000

    Add GH url

--- src/App.js
+++ src/App.js
@@ -2,5 +2,20 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">React Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project is created by Igor Zevaka and is{" "}
+        <a
+          href="https://github.com/izevaka/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on Github
+        </a>
+        .
+      </footer>
+    </div>
+  );
 }
